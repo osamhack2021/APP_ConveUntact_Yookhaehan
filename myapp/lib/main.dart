115c49cbcb8b3fc23b51ae79978a0c344ebcf208
zho <71474242+zhoho@users.noreply.github.com>
2021-10-04 04:18:56 +0000

    change

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
+const primaryColor = Color(0xFFACBDF4);
 void main() {
   runApp(MaterialApp(
-    title: '버튼 종류',
+    theme: ThemeData(
+      scaffoldBackgroundColor: Color(0xFFACBDF4),
+      primaryColor: primaryColor,
+    ),
+    title: '부대 선택',
     home: Scaffold(
       appBar: AppBar(
-        title: Text('버튼 종류'),
+        backgroundColor: primaryColor,
+        title: Text(''),
+        automaticallyImplyLeading: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_new),
+          onPressed: () {},
+        ),
       ),
       body: MyApp(),
     ),
@@ -20,10 +31,34 @@ class MyApp extends StatelessWidget {
     ctx = context;
     return Center(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           InkWell(
+            child: InkWell(
+              child: Image.asset(
+                  '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image.png',
+                  width: 120,
+                  height: 120),
+              onTap: () => showMessage('ImageButton'),
+            ),
+          ),
+          InkWell(
             child: Image.asset(
-                'https://files.slack.com/files-pri/T02DEDTJZ1R-F02GABHH338/image.png',
+                '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image(1).png',
+                width: 120,
+                height: 120),
+            onTap: () => showMessage('ImageButton'),
+          ),
+          InkWell(
+            child: Image.asset(
+                '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image(3).png',
+                width: 120,
+                height: 120),
+            onTap: () => showMessage('ImageButton'),
+          ),
+          InkWell(
+            child: Image.asset(
+                '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image(2).png',
                 width: 120,
                 height: 120),
             onTap: () => showMessage('ImageButton'),

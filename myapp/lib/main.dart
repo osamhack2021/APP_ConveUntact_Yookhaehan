7c095da7cabce12213603a0bf53e8aef6b1873e9
zho <71474242+zhoho@users.noreply.github.com>
2021-10-04 05:52:48 +0000

    사용자관리자회원가입구현

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -11,7 +11,7 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         backgroundColor: primaryColor,
-        title: Text(''),
+        title: Text('부대선택 페이지'),
         automaticallyImplyLeading: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios_new),
@@ -29,42 +29,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ctx = context;
-    return Center(
+    return Container(
+      margin: const EdgeInsets.all(30),
+      padding: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          width: 1.0,
+          color: Colors.white,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          InkWell(
-            child: InkWell(
-              child: Image.asset(
-                  '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image.png',
-                  width: 120,
-                  height: 120),
-              onTap: () => showMessage('ImageButton'),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Column(
+              children: <Widget>[
+                InkWell(
+                  child: InkWell(
+                    child: Image.asset(
+                        '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image.png',
+                        width: 180,
+                        height: 180),
+                    onTap: () => showMessage('육군'),
+                  ),
+                ),
+                InkWell(
+                  child: Image.asset(
+                      '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image(1).png',
+                      width: 180,
+                      height: 180),
+                  onTap: () => showMessage('해군'),
+                ),
+                InkWell(
+                  child: Image.asset(
+                      '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image(3).png',
+                      width: 180,
+                      height: 180),
+                  onTap: () => showMessage('공군'),
+                ),
+                InkWell(
+                  child: Image.asset(
+                      '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image(2).png',
+                      width: 180,
+                      height: 180),
+                  onTap: () => showMessage('해병대'),
+                ),
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             ),
           ),
-          InkWell(
-            child: Image.asset(
-                '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image(1).png',
-                width: 120,
-                height: 120),
-            onTap: () => showMessage('ImageButton'),
-          ),
-          InkWell(
-            child: Image.asset(
-                '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image(3).png',
-                width: 120,
-                height: 120),
-            onTap: () => showMessage('ImageButton'),
-          ),
-          InkWell(
-            child: Image.asset(
-                '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/image(2).png',
-                width: 120,
-                height: 120),
-            onTap: () => showMessage('ImageButton'),
-          ),
         ],
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       ),
     );
   }

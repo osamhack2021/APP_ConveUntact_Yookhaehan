70154d39fda8535191430584716e874da6ee94ec
zho <71474242+zhoho@users.noreply.github.com>
2021-10-18 23:23:02 +0000

    edit_readme/error

--- myapp/lib/components/user_info.dart
+++ myapp/lib/components/user_info.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class User{
+class User {
   Image? profile;
   String unit;
   String company;
@@ -9,28 +9,30 @@ class User{
   String id;
   String email;
   String pw;
+  String unitcode;
 
-  User({
-    this.profile,
-    required this.unit,
-    required this.company,
-    required this.rank,
-    required this.name,
-    required this.id,
-    required this.email,
-    required this.pw,
-  });
+  User(
+      {this.profile,
+      required this.unit,
+      required this.company,
+      required this.rank,
+      required this.name,
+      required this.id,
+      required this.email,
+      required this.pw,
+      required this.unitcode});
 }
 
 List<User> myInfo = [
   User(
-    profile: null,//Image.asset('/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/icons/soldier.png'),
-    unit: "X군단 X단 X대대",
-    company: "X 중대",
-    rank: "일병",
-    name: "홍길동",
-    id: "20-11111111",
-    email: "user@gmail.com",
-    pw: "1234"
-  ),
-]; 
+      profile:
+          null, //Image.asset('/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/icons/soldier.png'),
+      unit: "X군단 X단 X대대",
+      company: "X 중대",
+      rank: "일병",
+      name: "홍길동",
+      id: "20-11111111",
+      email: "user@gmail.com",
+      unitcode: "1d341a",
+      pw: "1234"),
+];
--- myapp/lib/pages/admin/ad_my_page/ad_mypage.dart
+++ myapp/lib/pages/admin/ad_my_page/ad_mypage.dart
@@ -1,4 +1,4 @@
-import 'package:cool_alert/cool_alert.dart';   
+import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 import 'package:myapp/components/company_info.dart';
@@ -13,6 +13,7 @@ class ADMyPageScreen extends StatefulWidget {
   @override
   ADMyInfoList createState() => ADMyInfoList();
 }
+
 class ADMyInfoList extends State<ADMyPageScreen> {
   @override
   Widget build(BuildContext context) {
@@ -21,19 +22,14 @@ class ADMyInfoList extends State<ADMyPageScreen> {
       headerWidget: headerWidget(context),
       //headerBottomBar: headerBottomBarWidget(),
       body: [
-        Row(
-          children: [
-            Padding(padding: EdgeInsets.all(10)),
-            Text(
-              "개인정보",
+        Row(children: [
+          Padding(padding: EdgeInsets.all(10)),
+          Text("개인정보",
               style: TextStyle(
-                color: Colors.grey,
-                fontWeight: FontWeight.bold,
-                fontSize: 20
-              )
-            ),
-          ]
-        ),
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20)),
+        ]),
         listView(),
       ],
       //fullyStretchable: true,
@@ -42,210 +38,224 @@ class ADMyInfoList extends State<ADMyPageScreen> {
   }
 
   //Container headerBottomBarWidget() {
-    //return Container(
-      //child: Row(
-        //mainAxisSize: MainAxisSize.min,
-        //mainAxisAlignment: MainAxisAlignment.end,
-        //crossAxisAlignment: CrossAxisAlignment.center,
-        //children: [//설정 아이콘
-          //IconButton(onPressed: () {}, icon: Icon(Icons.person, color: Colors.white),),
-        //],
-      //),
-    //);
+  //return Container(
+  //child: Row(
+  //mainAxisSize: MainAxisSize.min,
+  //mainAxisAlignment: MainAxisAlignment.end,
+  //crossAxisAlignment: CrossAxisAlignment.center,
+  //children: [//설정 아이콘
+  //IconButton(onPressed: () {}, icon: Icon(Icons.person, color: Colors.white),),
+  //],
+  //),
+  //);
   //}
 
   Container headerWidget(BuildContext context) => Container(
-    color: Colors.indigo.shade200,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        CircleAvatar(
-          radius: 70,
-          backgroundColor: Colors.white,
-          child: myInfo[0].profile == null ? Image.asset(
-            '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/images/army.png',
-            width: 100,
-            height: 100
-          ) : myInfo[0].profile,
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        color: Colors.indigo.shade200,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text(
-              "관리자",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                //letterSpacing: 0.5,
-                fontSize: 30,
-              ),
+            CircleAvatar(
+              radius: 70,
+              backgroundColor: Colors.white,
+              child: myInfo[0].profile == null
+                  ? Image.asset(
+                      '/workspaces/APP_ConveUntact_Yookhaehan/myapp/lib/images/army.png',
+                      width: 100,
+                      height: 100)
+                  : myInfo[0].profile,
             ),
-            Text(
-              "${myInfo[0].unit} ${myInfo[0].company}",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 0.5,
-                fontSize: 20,
-              ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "관리자",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    //letterSpacing: 0.5,
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  "${myInfo[0].unit} ${myInfo[0].company}",
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 0.5,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
-      ],
-    ),
-  );
-
+      );
 
   ListView listView() {
-    String input ="";
+    String input = "";
     return ListView(
       padding: EdgeInsets.only(top: 0),
       physics: NeverScrollableScrollPhysics(),
       shrinkWrap: true,
       children: <Widget>[
         ListTile(
-          title: Text("소속", style: TextStyle(color: Colors.indigo.shade200, fontWeight: FontWeight.bold)),
-          subtitle: Text(myInfo[0].unit, textAlign: TextAlign.right,),
-          onTap: (){
-            input = "";
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text("변경할 소속명 입력"),
-                  content: TextField(
-                    onChanged: (String value) {
-                      input = value;
-                    },
-                  ),
-                  actions: <Widget>[
-                    FlatButton(
-                      onPressed: () {
-                        setState(() {
-                          myInfo[0].unit = input;
-                        });
-                        Navigator.of(context).pop(); // input 입력 후 창 닫히도록
-                      },
-                      child: Text("변경하기")
-                    ),
-                    //RaisedButton(
-                      //onPressed: () {
+            title: Text("소속",
+                style: TextStyle(
+                    color: Colors.indigo.shade200,
+                    fontWeight: FontWeight.bold)),
+            subtitle: Text(
+              myInfo[0].unit,
+              textAlign: TextAlign.right,
+            ),
+            onTap: () {
+              input = "";
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("변경할 소속명 입력"),
+                      content: TextField(
+                        onChanged: (String value) {
+                          input = value;
+                        },
+                      ),
+                      actions: <Widget>[
+                        FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                myInfo[0].unit = input;
+                              });
+                              Navigator.of(context).pop(); // input 입력 후 창 닫히도록
+                            },
+                            child: Text("변경하기")),
+                        //RaisedButton(
+                        //onPressed: () {
                         //print(teamFacilityList);
                         //Get.to(ADFacilityModifyPage());
-                      //},
-                      //child: Text('저장하기')
-                    //),
-                  ],
-                );
-              }
-            );
-          }
+                        //},
+                        //child: Text('저장하기')
+                        //),
+                      ],
+                    );
+                  });
+            }),
+        ListTile(
+          title: Text("예하부대",
+              style: TextStyle(
+                  color: Colors.indigo.shade200, fontWeight: FontWeight.bold)),
+          trailing: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Get.to(() => AddDetail());
+              },
+              child: Text('예하부대 변경'),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Colors.indigo.shade200),
+              ),
+            ),
+          ]),
         ),
         ListTile(
-          title: Text("예하부대", style: TextStyle(color: Colors.indigo.shade200, fontWeight: FontWeight.bold)),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: (){
-                  Get.to(() => AddDetail());
-                },
-                child: Text('예하부대 변경'),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.indigo.shade200),
-                ),
-              ),
-            ]
+          title: Text("이름",
+              style: TextStyle(
+                  color: Colors.indigo.shade200, fontWeight: FontWeight.bold)),
+          subtitle: Text(
+            "관리자",
+            textAlign: TextAlign.right,
           ),
         ),
         ListTile(
-          title: Text("이름", style: TextStyle(color: Colors.indigo.shade200, fontWeight: FontWeight.bold)),
-          subtitle: Text("관리자", textAlign: TextAlign.right,),
-        ),
-        ListTile(
-          title: Text("부대 코드번호", style: TextStyle(color: Colors.indigo.shade200, fontWeight: FontWeight.bold)),
-          subtitle: Text(myInfo[0].id, textAlign: TextAlign.right,),
-          onTap: (){
-            input = "";
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text("변경할 군번 입력"),
-                  content: TextField(
-                    onChanged: (String value) {
-                      input = value;
-                    },
-                  ),
-                  actions: <Widget>[
-                    FlatButton(
-                      onPressed: () {
-                        setState(() {
-                          myInfo[0].id = input;
-                        });
-                        Navigator.of(context).pop(); // input 입력 후 창 닫히도록
-                      },
-                      child: Text("변경하기")
-                    ),
-                    //RaisedButton(
-                      //onPressed: () {
+            title: Text("부대 코드번호",
+                style: TextStyle(
+                    color: Colors.indigo.shade200,
+                    fontWeight: FontWeight.bold)),
+            subtitle: Text(
+              myInfo[0].unitcode,
+              textAlign: TextAlign.right,
+            ),
+            onTap: () {
+              input = "";
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("변경할 부대코드번호 입력"),
+                      content: TextField(
+                        onChanged: (String value) {
+                          input = value;
+                        },
+                      ),
+                      actions: <Widget>[
+                        FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                myInfo[0].id = input;
+                              });
+                              Navigator.of(context).pop(); // input 입력 후 창 닫히도록
+                            },
+                            child: Text("변경하기")),
+                        //RaisedButton(
+                        //onPressed: () {
                         //print(teamFacilityList);
                         //Get.to(ADFacilityModifyPage());
-                      //},
-                      //child: Text('저장하기')
-                    //),
-                  ],
-                );
-              }
-            );
-          }
-        ),
+                        //},
+                        //child: Text('저장하기')
+                        //),
+                      ],
+                    );
+                  });
+            }),
         ListTile(
-          title: Text("이메일", style: TextStyle(color: Colors.indigo.shade200, fontWeight: FontWeight.bold)),
-          subtitle: Text(myInfo[0].email, textAlign: TextAlign.right,),
-          onTap: (){
-            input = "";
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text("변경할 이메일 입력"),
-                  content: TextField(
-                    onChanged: (String value) {
-                      input = value;
-                    },
-                  ),
-                  actions: <Widget>[
-                    FlatButton(
-                      onPressed: () {
-                        setState(() {
-                          myInfo[0].email = input;
-                        });
-                        Navigator.of(context).pop(); // input 입력 후 창 닫히도록
-                      },
-                      child: Text("변경하기")
-                    ),
-                    //RaisedButton(
-                      //onPressed: () {
+            title: Text("이메일",
+                style: TextStyle(
+                    color: Colors.indigo.shade200,
+                    fontWeight: FontWeight.bold)),
+            subtitle: Text(
+              myInfo[0].email,
+              textAlign: TextAlign.right,
+            ),
+            onTap: () {
+              input = "";
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("변경할 이메일 입력"),
+                      content: TextField(
+                        onChanged: (String value) {
+                          input = value;
+                        },
+                      ),
+                      actions: <Widget>[
+                        FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                myInfo[0].email = input;
+                              });
+                              Navigator.of(context).pop(); // input 입력 후 창 닫히도록
+                            },
+                            child: Text("변경하기")),
+                        //RaisedButton(
+                        //onPressed: () {
                         //print(teamFacilityList);
                         //Get.to(ADFacilityModifyPage());
-                      //},
-                      //child: Text('저장하기')
-                    //),
-                  ],
-                );
-              }
-            );
-          }
-        ),
+                        //},
+                        //child: Text('저장하기')
+                        //),
+                      ],
+                    );
+                  });
+            }),
         ListTile(
-          title: Text("비밀번호", style: TextStyle(color: Colors.indigo.shade200, fontWeight: FontWeight.bold)),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: (){
-                  input = "";
-                  showDialog(
+          title: Text("비밀번호",
+              style: TextStyle(
+                  color: Colors.indigo.shade200, fontWeight: FontWeight.bold)),
+          trailing: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                input = "";
+                showDialog(
                     context: context,
                     builder: (BuildContext context) {
                       return AlertDialog(
@@ -257,35 +267,34 @@ class ADMyInfoList extends State<ADMyPageScreen> {
                         ),
                         actions: <Widget>[
                           FlatButton(
-                            onPressed: () {
-                              setState(() {
-                                myInfo[0].pw = input;
-                              });
-                              Navigator.of(context).pop(); // input 입력 후 창 닫히도록
-                            },
-                            child: Text("변경하기")
-                          ),
+                              onPressed: () {
+                                setState(() {
+                                  myInfo[0].pw = input;
+                                });
+                                Navigator.of(context)
+                                    .pop(); // input 입력 후 창 닫히도록
+                              },
+                              child: Text("변경하기")),
                           //RaisedButton(
-                            //onPressed: () {
-                              //print(teamFacilityList);
-                              //Get.to(ADFacilityModifyPage());
-                            //},
-                            //child: Text('저장하기')
+                          //onPressed: () {
+                          //print(teamFacilityList);
+                          //Get.to(ADFacilityModifyPage());
+                          //},
+                          //child: Text('저장하기')
                           //),
                         ],
                       );
-                    }
-                  );
-                },
-                child: Text('비밀번호 변경'),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.indigo.shade200),
-                ),
+                    });
+              },
+              child: Text('비밀번호 변경'),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Colors.indigo.shade200),
               ),
-            ]
-          ),
+            ),
+          ]),
         ),
       ],
     );
   }
-} 
+}

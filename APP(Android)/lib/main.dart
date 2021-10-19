a3f18b69d6e3b3771f19dbd1136cdcb8a4ed67a3
zho <71474242+zhoho@users.noreply.github.com>
2021-10-19 23:01:08 +0000

    edit_name

--- APP(Android)/lib/main.dart
+++ APP(Android)/lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/pages/admin/ad_facility_modify/ad_facility_modify_menu.dart';
-import 'package:myapp/pages/admin/ad_homepage/ad_home_page_menu.dart';
-import 'package:myapp/pages/home_page/homepage_menu.dart';
 import 'package:myapp/pages/login/login_screen.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:myapp/pages/signup/admin/add_detail.dart';
-import 'package:myapp/pages/signup/admin/add_detail2.dart';
-import 'package:myapp/pages/signup/admin/admin_finish_signup.dart';
-import 'package:myapp/pages/signup/signtype.dart';
 
 import 'binding/app_binding.dart';
 
